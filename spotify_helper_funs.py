83b216efc058505c8c6fe31821e47bf3ce224b72
Izzy Beers <izzybeers@Izzys-MacBook-Pro.local>
2026-07-15 07:59:09 -0400

    add helper for getting readable playlists and fix bug in get_songs_on_playlist function

--- spotify_helper_funs.py
+++ spotify_helper_funs.py
@@ -12,19 +12,10 @@ CLIENT_ID = "4e8269446f474d3f920be9dfa56086d5"
 CLIENT_SECRET = "69f0f2b62b924d458d14a45a526cd5ed"
 REDIRECT_URI = "http://127.0.0.1:9090/callback" # Must match your Spotify Dashboard
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TOKEN_PATH = os.path.join(BASE_DIR, "spotify_token.txt")
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    redirect_uri=REDIRECT_URI,
-    scope="user-library-read user-read-recently-played",
-    cache_path="spotify_token.txt"
-), retries = 0)
-
 def get_song_info(uri_list, sp):
     all_track_info = pd.DataFrame()
     for song in uri_list:
+        print(song)
         track = sp.track(song)
         track_info = pd.DataFrame({
             'uri': [song],
@@ -79,9 +70,9 @@ def read_from_supabase(table_name, select = '*', eq_col_name = None, eq_value = 
         start_indx = 0
         while True:
             if eq_col_name is None:
-                res = pd.DataFrame(supabase.table(table_name).select(select).range(start_indx, start_indx + chunk_size).execute().data)
+                res = pd.DataFrame(supabase.table(table_name).select(select).range(start_indx, start_indx + chunk_size-1).execute().data)
             else:
-                res = pd.DataFrame(supabase.table(table_name).select(select).eq(eq_col_name, eq_value).range(start_indx, start_indx + chunk_size).execute().data)
+                res = pd.DataFrame(supabase.table(table_name).select(select).eq(eq_col_name, eq_value).range(start_indx, start_indx + chunk_size-1).execute().data)
             df_results = pd.concat([df_results, res])
             if len(res) < chunk_size:
                 break
@@ -94,25 +85,40 @@ def read_from_supabase(table_name, select = '*', eq_col_name = None, eq_value = 
         else:
             return(pd.DataFrame(supabase.table(table_name).select(select).eq(eq_col_name, eq_value).execute().data))
         
-def get_followed_playlists():
-    followed_playlists = []
+def get_followed_playlists(sp):
+    followed_playlists = pd.DataFrame()
     offset = 0
     while True:
         res = sp.current_user_playlists(limit = 50, offset = offset)
-        res_to_add = [item['id'] for item in res['items']]
-        followed_playlists = followed_playlists + res_to_add
+        res_to_add = [{'id': item['id'], 'name': item['name']} for item in res['items']]
+        followed_playlists = pd.concat([followed_playlists, pd.DataFrame(res_to_add)], axis = 0)
         if len(res_to_add) == 50:
             offset += 50
         else:
             break
     return followed_playlists
 
-def get_songs_on_playlist(playlist_id):
+def get_readable_playlists(sp):
+    followed_playlists = get_followed_playlists(sp)
+    readable = []
+    for f in range(len(followed_playlists)):
+        try:
+            get_first_song = sp.playlist_items(playlist_id = followed_playlists['id'].iloc[f], limit = 1, offset = 0)
+            readable.append({
+                'id': followed_playlists['id'].iloc[f],
+                'name': followed_playlists['name'].iloc[f]
+            })
+        except Exception as e:
+            pass
+    return pd.DataFrame(readable)
+
+def get_songs_on_playlist(playlist_id, sp):
     songs_on_playlist = pd.DataFrame()
     start_indx = 0
     while True:
         songs_on_playlist_dict = sp.playlist_items(playlist_id = playlist_id, limit = 50, offset = start_indx)
         res = [{'uri': item['item']['uri'],
+                'name': item['item']['name'],
                 'artist': item['item']['artists'][0]['name']} for item in songs_on_playlist_dict['items'] if item['item']['uri'] is not None]
         songs_on_playlist = pd.concat([songs_on_playlist, pd.DataFrame(res)])
         if len(res) == 50:
